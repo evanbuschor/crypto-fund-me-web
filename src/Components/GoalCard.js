1381cd545af39b54117fe16b2d62d41fcf8e0e95
evan <evan.buschor@gmail.com>
2021-03-23 21:57:57 -0400

    add basic posting to api server

--- src/Components/GoalCard.js
+++ src/Components/GoalCard.js
@@ -1,7 +1,15 @@
 import React from "react";
+import QRCode from "react-qr-code";
 
-const GoalCard = (props) => {
-    return <div>{props.title}</div>;
+const GoalCard = ({ id, title, addr, desc, amount, startDate, ...rest }) => {
+    return (
+        <div className="goal-card">
+            <h1>{title}</h1>
+            <h2>{`₿${amount} BTC  (${addr})`}</h2>
+            <QRCode value={addr} size="200"></QRCode>
+            <p>{desc}</p>
+        </div>
+    );
 };
 
 export default GoalCard;
--- src/Components/NewGoal.js
+++ src/Components/NewGoal.js
@@ -1,29 +1,71 @@
-import React from "react";
-
-function handleSubmit(e) {
-    e.preventDefault();
-    console.log("form submit", e);
-}
+import React, { useState, useEffect } from "react";
 
 const NewGoal = () => {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [addr, setAddr] = useState("");
+    const [amount, setAmount] = useState("");
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+            await fetch(`http://localhost:3000/goals/`, {
+                method: "POST",
+                body: JSON.stringify({ title, description, addr, amount }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="title">
                     Funding Goal Title:
-                    <input id="title" type="text" />
+                    <input
+                        onChange={(e) => {
+                            setTitle(e.target.value);
+                        }}
+                        id="title"
+                        type="text"
+                        value={title}
+                    />
                 </label>
-                <label htmlFor="desc">
+                <label htmlFor="description">
                     Description:
-                    <textarea id="desc" />
+                    <textarea
+                        id="description"
+                        onChange={(e) => {
+                            setDescription(e.target.value);
+                        }}
+                        value={description}
+                    />
                 </label>
-                <label htmlFor="wallet">
+                <label htmlFor="addr">
                     Wallet Address:
-                    <input id="wallet" type="text" />
+                    <input
+                        id="addr"
+                        type="text"
+                        onChange={(e) => {
+                            setAddr(e.target.value);
+                        }}
+                        value={addr}
+                    />
                 </label>
-                <label htmlFor="ammount">
-                    Goal ammount:
-                    <input id="ammount" type="text" />
+                <label htmlFor="amount">
+                    Goal amount:
+                    <input
+                        id="amount"
+                        type="text"
+                        onChange={(e) => {
+                            setAmount(e.target.value);
+                        }}
+                        value={amount}
+                    />
                 </label>
                 <button>submit</button>
             </form>
